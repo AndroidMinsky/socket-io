b695eac20fc812c644d8925ca2bd572b1d933dc1
AndroidMinsky <aleksandr.vesselko@gmail.com>
2023-01-15 20:45:32 +0000

    poll error fix

--- socket.js
+++ socket.js
@@ -1,8 +1,8 @@
 import { io } from "socket.io-client";
 
-// const URL = "https://socket-io-server-8mp0.onrender.com";
-const URL = "http://localhost:3001";
-const socket = io(URL, { autoConnect: false });
+const URL = "https://socket-io-server-8mp0.onrender.com";
+// const URL = "http://localhost:3001";
+const socket = io(URL, { autoConnect: false, transports: ["websocket"] });
 
 socket.onAny((event, ...args) => {
   console.log(event, args);
